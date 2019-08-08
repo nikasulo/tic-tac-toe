c666efb841411faff62809db025214542b4e9cc7
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-08-08 16:53:01 +0100

    Add show board method

--- game.rb
+++ game.rb
@@ -7,3 +7,20 @@ class Player
     end
 end
 
+class Board
+    def initialize
+        @state = ['','','','','','','','','','']
+        @move = 0
+    end
+
+    def show_board
+        p "#{@state[1]} | #{@state[2]} | #{@state[3]}"
+        p "---|---|---"
+        p "#{@state[4]} | #{@state[5]} | #{@state[6]}"
+        p "---|---|---"
+        p "#{@state[7]} | #{@state[8]} | #{@state[9]}"
+    end
+
+
+end
+
