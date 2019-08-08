8d62516e554ec43997bd1afc1416fd488feafe1f
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-08-08 20:01:23 +0100

    Add logic for the game play

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,7 @@
 class Player
     attr_reader :name, :symbol
 
-    def initialize
+    def initialize(name, symbol)
         @name = name
         @symbol = symbol
     end
@@ -9,7 +9,7 @@ end
 
 class Board
     def initialize
-        @state = ['','','','','','','','','','']
+        @state = ['',1,2,3,4,5,6,7,8,9]
         @move = 0
     end
 
@@ -22,12 +22,11 @@ class Board
     end
 
     def is_move_valid? current_position
-        @state[current_position].is_a? Integer
+        @state[current_position].is_a?Integer and current_position.is_a?Integer and (1..9).include?(current_position) 
     end
 
     def add_move(position,symbol)
         @state[position] = symbol
-        is_move_valid? position
         @move += 1
 
         if win?
@@ -44,10 +43,12 @@ class Board
             winning_diagonal? || winning_row? || winning_column?
         end
 
-        def winning_diagonal
+        def winning_diagonal?
             left_diagonal = [@state[1], @state[5], @state[9]].uniq.length
             right_diagonal = [@state[3], @state[5], @state[7]].uniq.length
-            [left_diagonal, right_diagonal].include? 1
+            if [left_diagonal, right_diagonal].include?(1)
+                true
+            end
         end
 
         def winning_column?
@@ -55,14 +56,18 @@ class Board
             right_column = [@state[3], @state[6], @state[9]].uniq.length
             center_column = [@state[2], @state[5], @state[8]].uniq.length
 
-            [left_column, center_column, right_column].include? 1
+            if [left_column, center_column, right_column].include?(1)
+                true
+            end
         end
 
         def winning_row?
             top_row = [@state[1], @state[2], @state[3]].uniq.length 
             center_row = [@state[4], @state[5], @state[6]].uniq.length 
-            bottom_row = [@state[7], @state[8]], @state[9]].uniq.length 
-            [top_row, bottom_row, center_column].include? 1
+            bottom_row = [@state[7], @state[8], @state[9]].uniq.length 
+            if [top_row, bottom_row, center_row].include?(1)
+                true
+            end
         end 
 
         def draw?
@@ -70,15 +75,3 @@ class Board
         end
 
 end
-
-class Game
-    def initialize
-        p 'Player 1, enter your name: '
-        @player1 = Player.new(gets.chomp, 'X')
-        p 'Player 2 enter your name: '
-        @player2 = Player.new(gets.chomp, 'O')
-        @board = Board.new
-    end
-end
-
-
