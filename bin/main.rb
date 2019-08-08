4238c67b5b2fe978dc76f591d4e1ef591751dc04
Tolulope Oluwadamilare Olusakin <oluwadamilareo@fixus.com.ng>
2019-08-08 17:36:29 +0100

    Revert "Add Board Class"

--- bin/main.rb
+++
@@ -1,9 +0,0 @@
-class Game
-    def initialize
-        p 'Player 1, enter your name: '
-        @player1 = Player.new(gets.chomp, 'X')
-        p 'Player 2 enter your name: '
-        @player2 = Player.new(gets.chomp, 'O')
-        @board = Board.new
-    end
-end
