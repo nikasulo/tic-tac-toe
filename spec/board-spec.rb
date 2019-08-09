a280dbca843c10fa57d4e20f76ec21ba71a2b381
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-08-09 21:46:28 +0100

    Add move rejects invalid moves, show board prints 3 rows

--- spec/board-spec.rb
+++ spec/board-spec.rb
@@ -1,20 +1,27 @@
 require '../lib/game.rb'
 
-<<<<<<< HEAD
 describe '::Board' do
   let(:board) {Board.new}
   let(:show_board) {board.show_board}
   
+  
   describe '#show_board' do
-    it 'prints the board' do
-      expect(show_board).to eql(' 7 | 8 | 9 ')
+
+    it 'prints the first row' do
+      show_board
+      expect(board.first_row).to eql(' 1 | 2 | 3 ')
     end
-=======
-describe '#show_board' do
-  it 'prints the arrangment of the board' do
-    board = Board.new = board.show_board
-    expect (@state).to eql(Array(1..9))
->>>>>>> f175e7e6da3f81d9b93cda3c408c8189c323a8bd
+
+    it 'prints the second row' do
+      show_board
+      expect(board.second_row).to eql(' 4 | 5 | 6 ')
+    end
+
+    it 'prints third row' do
+      show_board
+      expect(board.third_row).to eql(' 7 | 8 | 9 ')
+    end
+    
   end
 
   describe "#is_move_valid?" do
@@ -25,6 +32,21 @@ describe '#show_board' do
 
     it 'rejects invalid moves' do
       expect(board.is_move_valid? 'a').to eql(false)
+    end
+  end
+
+  describe "#add_move" do 
+    it 'adds moves' do
+      expect(board.add_move(1, 'X')).to be 0
+    end
+
+    it 'rejects invalid move' do
+      expect(board.add_move('r', 'X')).to be nil
+    end
+    
+    it 'updates the state' do
+      board.add_move(1, 'X')
+      expect(board.state[1]).to eql("X")
     end
   end
 end 
